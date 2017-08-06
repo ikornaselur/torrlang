dcad1bf85a79c5309925dee743e07a8949543ddd
Axel Örn Sigurðsson <axel@takumi.com>
2017-08-06 14:16:12 +0000

    Update bencoding:decode to binary in same form as encode wants it

--- bencoding.erl
+++ bencoding.erl
@@ -29,35 +29,40 @@ encode_key_value(Key, Value) ->
 
 
 %%% Decoding
-decode([H|T]) when H > $0, H =< $9 ->
+decode(String) ->
+  {Result, []} = bdecode(String),
+  Result.
+
+bdecode([H|T]) when H > $0, H =< $9 ->
   % Binary string
   {KeyLength, [_|RestWithKey]} = lists:splitwith(fun(C) -> C =/= $: end, [H|T]),
-  lists:split(list_to_integer(KeyLength), RestWithKey);
-decode([H|T]) when H =:= $i ->
+  {Binary, Rest} = lists:split(list_to_integer(KeyLength), RestWithKey),
+  {{binary, Binary}, Rest};
+bdecode([H|T]) when H =:= $i ->
   % Integer
   {Number, [_|Rest]} = lists:splitwith(fun(C) -> C =/= $e end, T),
   {list_to_integer(Number), Rest};
-decode([H|T]) when H =:= $l ->
+bdecode([H|T]) when H =:= $l ->
   % List
-  decode_list([], T);
-decode([H|T]) when H =:= $d ->
+  bdecode_list([], T);
+bdecode([H|T]) when H =:= $d ->
   % Dictionary
-  decode_dict(maps:new(), T).
+  bdecode_dict(maps:new(), T).
 
 % Parse dictionaries
-decode_dict(Dict, [H|Rest]) when H =:= $e ->
+bdecode_dict(Dict, [H|Rest]) when H =:= $e ->
   % Dictionary ends with $e, so simply return the constructed dictionary
   {Dict, Rest};
-decode_dict(Dict, Payload) ->
+bdecode_dict(Dict, Payload) ->
   % Update the Dict with a parse Key + Value
-  {Key, RestWithValue} = decode(Payload),
-  {Value, Rest} = decode(RestWithValue),
-  decode_dict(maps:put(Key, Value, Dict), Rest).
+  {Key, RestWithValue} = bdecode(Payload),
+  {Value, Rest} = bdecode(RestWithValue),
+  bdecode_dict(maps:put(Key, Value, Dict), Rest).
 
 % Parse lists
-decode_list(List, [H|Rest]) when H =:= $e ->
+bdecode_list(List, [H|Rest]) when H =:= $e ->
   % Lists end with $e, so simply return the constructed list
   {List, Rest};
-decode_list(List, Payload) ->
-  {Item, Rest} = decode(Payload),
-  decode_list([Item|List], Rest).
+bdecode_list(List, Payload) ->
+  {Item, Rest} = bdecode(Payload),
+  bdecode_list([Item|List], Rest).
--- test.erl
+++ test.erl
@@ -1,5 +1,6 @@
 -module(test).
 -export([test/0]).
+-include_lib("stdlib/include/assert.hrl").
 
 test() ->
   String = "d3:key5:value4:key2d2:ye2:yoe4:listl2:hei25e2:ho2:hiee",
@@ -20,4 +21,16 @@ test() ->
 
   Dict = maps:from_list([{{binary, "list"}, List}, {{binary, "key"}, {binary, "value"}}]),
   io:format("Testing dict encoding: ~p~n", [Dict]),
-  io:format("~p~n~n", [bencoding:encode(Dict)]).
+  io:format("~p~n~n", [bencoding:encode(Dict)]),
+
+  % Test both directions
+  io:format("Testing string -> encoding -> decoding~n", []),
+  Decoded = bencoding:decode(String),
+  Encoded = bencoding:encode(Decoded),
+  io:format("~p~n", [String]),
+  io:format("~p~n", [Encoded]),
+  Result = case length(String) =:= length(Encoded) of
+    true -> ok;
+    _ -> fail
+  end,
+  io:format("Success (checked length): ~p~n", [Result]).
