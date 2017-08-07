78aad2ebea1c25956e7d1e5e6a95e53ab21a66a4
Axel Örn Sigurðsson <axel@takumi.com>
2017-08-07 16:45:42 +0000

    Refactor bencoding to be a little bit more readable

--- bencoding.erl
+++ bencoding.erl
@@ -3,54 +3,69 @@
 
 % Bencoding module
 %
-% Exposes functions to encode and decode to and from bencoding
+% Exposes functions to decode bencoded strings and
+% encode objects into bencoded strings
 
 
-%%% Encoding
+%%%%%%%%%%%%%%%%
+%%% Encoding %%%
+%%%%%%%%%%%%%%%%
 encode(List) when is_list(List) ->
   Internal = lists:flatmap(fun (Elm) -> encode(Elm) end, List),
   lists:append(["l", Internal, "e"]);
+
 encode(Dict) when is_map(Dict) ->
   Internal = encode_dict(Dict),
   lists:append(["d", Internal, "e"]);
+
 encode(Int) when is_integer(Int) ->
   lists:append(["i", integer_to_list(Int), "e"]);
+
 encode({binary, Bin}) ->
   BinaryLength = length(Bin),
   lists:append([integer_to_list(BinaryLength), ":", Bin]);
+
 encode(_) ->
   erlang:error(bencoding_unknown_item_to_encode).
+
 
 encode_dict(Dict) ->
   List = maps:to_list(Dict),
   lists:flatmap(fun ({Key, Value}) -> encode_key_value(Key, Value) end, List).
+
 encode_key_value(Key, Value) ->
   lists:append([encode(Key), encode(Value)]).
 
-
-%%% Decoding
+%%%%%%%%%%%%%%%%
+%%% Decoding %%%
+%%%%%%%%%%%%%%%%
 decode(String) ->
   {Result, []} = bdecode(String),
   Result.
+
 
 bdecode([H|T]) when H > $0, H =< $9 ->
   % Binary string
   {KeyLength, [_|RestWithKey]} = lists:splitwith(fun(C) -> C =/= $: end, [H|T]),
   {Binary, Rest} = lists:split(list_to_integer(KeyLength), RestWithKey),
   {{binary, Binary}, Rest};
-bdecode([H|T]) when H =:= $i ->
+
+bdecode([$i|T]) ->
   % Integer
   {Number, [_|Rest]} = lists:splitwith(fun(C) -> C =/= $e end, T),
   {list_to_integer(Number), Rest};
-bdecode([H|T]) when H =:= $l ->
+
+bdecode([$l|T]) ->
   % List
   bdecode_list([], T);
-bdecode([H|T]) when H =:= $d ->
+
+bdecode([$d|T]) ->
   % Dictionary
   bdecode_dict(maps:new(), T).
 
+
 % Parse dictionaries
-bdecode_dict(Dict, [H|Rest]) when H =:= $e ->
+bdecode_dict(Dict, [$e|Rest]) ->
   % Dictionary ends with $e, so simply return the constructed dictionary
   {Dict, Rest};
 bdecode_dict(Dict, Payload) ->
@@ -59,8 +74,9 @@ bdecode_dict(Dict, Payload) ->
   {Value, Rest} = bdecode(RestWithValue),
   bdecode_dict(maps:put(Key, Value, Dict), Rest).
 
+
 % Parse lists
-bdecode_list(List, [H|Rest]) when H =:= $e ->
+bdecode_list(List, [$e|Rest]) ->
   % Lists end with $e, so simply return the constructed list
   {List, Rest};
 bdecode_list(List, Payload) ->
